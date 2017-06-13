4eb09f8ab8d0819cd91c503ae55648bb630c5bd2
Nathan Holloway <nsh2501@gmail.com>
2017-06-13 11:33:14 -0500

    added staging to podList

--- functions/podlist.rb
+++ functions/podlist.rb
@@ -1,24 +1,36 @@
 #!/usr/bin/env ruby
 
-def f_pod_list ()
-  podList = ['d0p1', 
-	   'd0p2',
-           'd2p3',
-           'd3p4',
-           'd4p5',
-           'd5p6',
-           'd7p7',
-           'd8p8',
-	   'd0p9',
-           'd9p10',
-           'd3p12',
-           'd2p13',
-           'd4p14',
-           'd10p15',
-           'd11p16',
-           'd3p17',
-           'd12p21',
-           'd0p23'
-         ]
+#functions
+def f_pod_list (domain)
+
+  case domain
+  when 'prod'
+    podList = [
+      'd0p1', 
+      'd0p2',
+      'd2p3',
+      'd3p4',
+      'd4p5',
+      'd5p6',
+      'd7p7',
+      'd8p8',
+      'd0p9',
+      'd9p10',
+      'd3p12',
+      'd2p13',
+      'd4p14',
+      'd10p15',
+      'd11p16',
+      'd3p17',
+      'd12p21',
+      'd0p23'
+    ]
+  when 'stage'
+    podList = [
+      'd2p2',
+      'd4p1'
+    ]
+  end
+  
   podList
 end
