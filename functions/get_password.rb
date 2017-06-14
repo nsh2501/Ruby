1907ed4bc17b62a992db1f2f9ebbc4f2a65b9675
Nathan Holloway <nsh2501@gmail.com>
2017-06-14 12:24:53 -0500

    updated get_password for secret server

--- functions/get_password.rb
+++ functions/get_password.rb
@@ -1,8 +1,15 @@
-def get_password(resource, username)
-  resource_pass = `/tools-export/Scripts/functions/pmpcli_rest #{resource} #{username}`.chomp
-  if resource_pass == ""
-    #puts "Password not found for #{resource}, using default.".yellow
-    resource_pass = 'm0n3yb0vin3'
+require 'vmware_secret_server'
+
+def get_password(adpass, secret, ss_url, domain)
+  ss_connection = Vmware_secret_server::Session.new(ss_url, 'ad', adpass)
+  ss_password = ss_connection.get_password(secret)
+  if ss_password.is_a? Exception
+    clear_line
+    puts '[ ' + 'ERROR'.red + " ] Could not get password for #{secret} in Secret Server. Error is #{ss_password.message}"
+    return 'ERROR'
+  else 
+    clear_line
+    print '[ ' + 'INFO'.green + " ] Successfully pulled password from Secret Server for #{secret}"
+    return ss_password
   end
-  return resource_pass
 end
