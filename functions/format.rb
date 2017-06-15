413417ff2a9f2e581ef7495bbb3d0c8bc1236d94
Nathan <nsh2501@gmail.com>
2017-06-15 18:36:45 +0000

    fixed format.rb

--- functions/format.rb
+++ functions/format.rb
@@ -1,9 +1,7 @@
 require 'colorize'
 
-class Format
-  def clear_line
-    print "\r"
-    print "                                                                                                                                       "
-    print "\r"
-  end
+def clear_line ()
+  print "\r"
+  print "                                                                                                                                       "
+  print "\r"
 end
