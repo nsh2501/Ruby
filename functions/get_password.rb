2514f5048f5535be12df1bbff6bd75dae51964d7
Nathan <nsh2501@gmail.com>
2017-06-15 18:27:57 +0000

    updating files

--- functions/get_password.rb
+++ functions/get_password.rb
@@ -1,6 +1,8 @@
 require 'vmware_secret_server'
+require_relative '/home/nholloway/scripts/Ruby/functions/format.rb'
 
 def get_password(adpass, secret, domain)
+  format = Format.new
   case domain
   when 'prod'
     ss_url = "https://d0p1oss-mgmt-secret-web0.prod.vpc.vmw/SecretServer/webservices/SSWebservice.asmx?wsdl"
@@ -12,11 +14,11 @@ def get_password(adpass, secret, domain)
   ss_connection = Vmware_secret_server::Session.new(ss_url, 'ad', adpass)
   ss_password = ss_connection.get_password(secret)
   if ss_password.is_a? Exception
-    clear_line
+    format.clear_line
     puts '[ ' + 'ERROR'.red + " ] Could not get password for #{secret} in Secret Server. Error is #{ss_password.message}"
     return 'ERROR'
   else 
-    clear_line
+    format.clear_line
     print '[ ' + 'INFO'.green + " ] Successfully pulled password from Secret Server for #{secret}"
     return ss_password
   end
