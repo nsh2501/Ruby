9a88de015695b0a8727c7daacf7aeddcf1af8995
Nathan Holloway <nsh2501@gmail.com>
2017-07-06 13:19:55 -0500

    testing new get_vmhosts method

--- functions/rbvmomi_methods.rb
+++ functions/rbvmomi_methods.rb
@@ -121,3 +121,146 @@ def get_inv_info(vim, dc, host_prop=nil, vm_prop=nil)
 
   result = vim.serviceContent.propertyCollector.RetrieveProperties(:specSet => [filterSpec])
 end
+
+
+def get_vmhosts(vim, dc, host_prop=nil)
+  host_prop = %w(name parent runtime.connectionState) unless !host_prop.nil?
+
+  filterSpec = RbVmomi::VIM.PropertyFilterSpec(
+        :objectSet => [
+          :obj => dc,
+          :selectSet => [
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsFolder',
+              :type => 'Folder',
+              :path => 'childEntity',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsFolder'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsDatacenterHostFolder'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsClusterHost'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsVapp'),
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsDatacenterHostFolder',
+              :type => 'Datacenter',
+              :path => 'hostFolder',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsClusterHost',
+              :type => 'ClusterComputeResource',
+              :path => 'host',
+              :skip => false,
+              :selectSet => []
+            ),
+          ]
+        ],
+        :propSet => [
+          { :type => 'Folder', :pathSet => ['name', 'parent'] },
+          { :type => 'Datacenter', :pathSet => ['name', 'parent'] },
+          { :type => 'ClusterComputeResource', 
+            :pathSet => %w(name parent summary.effectiveCpu summary.effectiveMemory) 
+          },
+          { :type => 'HostSystem', :pathSet => host_prop },
+        ]
+      )
+
+
+  result = vim.serviceContent.propertyCollector.RetrieveProperties(:specSet => [filterSpec])
+enddef get_inv_info(vim, dc, host_prop=nil, vm_prop=nil)
+  host_prop = %w(name parent runtime.connectionState) unless !host_prop.nil?
+  vm_prop = %w(name runtime.powerState summary.guest.toolsStatus) unless !vm_prop.nil?
+
+  filterSpec = RbVmomi::VIM.PropertyFilterSpec(
+        :objectSet => [
+          :obj => dc,
+          :selectSet => [
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsFolder',
+              :type => 'Folder',
+              :path => 'childEntity',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsFolder'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsDatacenterVmFolder'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsDatacenterHostFolder'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsClusterRP'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsClusterHost'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsVapp'),
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsDatacenterVmFolder',
+              :type => 'Datacenter',
+              :path => 'vmFolder',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsDatacenterHostFolder',
+              :type => 'Datacenter',
+              :path => 'hostFolder',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsFolder')
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsClusterRP',
+              :type => 'ClusterComputeResource',
+              :path => 'resourcePool',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsRP'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsVapp'),
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsClusterHost',
+              :type => 'ClusterComputeResource',
+              :path => 'host',
+              :skip => false,
+              :selectSet => []
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsRP',
+              :type => 'ResourcePool',
+              :path => 'resourcePool',
+              :skip => false,
+              :selectSet => [
+                RbVmomi::VIM.SelectionSpec(:name => 'tsRP'),
+                RbVmomi::VIM.SelectionSpec(:name => 'tsVapp'),
+              ]
+            ),
+            RbVmomi::VIM.TraversalSpec(
+              :name => 'tsVapp',
+              :type => 'VirtualApp',
+              :path => 'vm',
+              :skip => false,
+              :selectSet => []
+            ),
+          ]
+        ],
+        :propSet => [
+          { :type => 'Folder', :pathSet => ['name', 'parent'] },
+          { :type => 'Datacenter', :pathSet => ['name', 'parent'] },
+          { :type => 'ClusterComputeResource', 
+            :pathSet => %w(name parent summary.effectiveCpu summary.effectiveMemory) 
+          },
+          { :type => 'ResourcePool', :pathSet => ['name', 'parent'] },
+          { :type => 'VirtualApp', :pathSet => ['name', 'parent', 'vm']},
+          { :type => 'HostSystem', :pathSet => host_prop },
+          { :type => 'VirtualMachine', :pathSet => vm_prop },
+        ]
+      )
+
+
+  result = vim.serviceContent.propertyCollector.RetrieveProperties(:specSet => [filterSpec])
+end
