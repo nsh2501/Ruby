976cd341d8e378c1b4819bdf26fff20d2e55a704
Nathan Holloway <nsh2501@gmail.com>
2017-09-05 09:11:11 -0500

    Created new file for loading functions

---
+++ functions/load_functions.rb
@@ -0,0 +1,6 @@
+#this file loads all new functions for pry session
+require '/home/nholloway/scripts/Ruby/functions/rbvmomi_methods.rb'
+require '/home/nholloway/scripts/Ruby/functions/format.rb'
+require '/home/nholloway/scripts/Ruby/functions/password_functions.rb'
+require '/home/nholloway/scripts/Ruby/functions/podlist.rb'
+require '/home/nholloway/scripts/Ruby/functions/vcenter_list_v2.rb'
