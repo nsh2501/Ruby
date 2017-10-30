24558a5ad8d0da6f191a2bbda864bed4cfa83771
Nathan Holloway <nsh2501@gmail.com>
2017-10-30 14:22:47 -0500

    Added ability to put in multiple VMs

--- takeSnapshot.rb
+++ takeSnapshot.rb
@@ -9,7 +9,7 @@ require_relative '/home/nholloway/scripts/Ruby/functions/format.rb'
 
 #params
 opts = Trollop::options do
-  opt :vm, "VM you want to snapshot", :type => :string, :required => true
+  opt :vm, "VM you want to snapshot", :type => :strings, :required => true
   opt :vcenter, "vCenter that the VM is on", :type => :string, :requied => true
   opt :snapshot_memory, "Whether or not to snapshot the memory", :type => :string, :requied => false, :default => 'true'
   opt :quiesce_filesystem, "Where or not to quiesce the filesystem", :type => :string, :required => false, :default => 'false'
@@ -31,8 +31,8 @@ domain = ENV['HOSTNAME'].split('.')[1]
 logger = config_logger(opts[:log_level].upcase, script_name)
 
 #options to debug logs
-logger.debug "DEBUG - opts: #{opts}"
-logger.debug "DEBUG - User: #{ad_user}"
+logger.info "INFO - opts: #{opts}"
+logger.debug "INFO - User: #{ad_user}"
 
 #log into vCenter
 clear_line
@@ -41,25 +41,27 @@ print '[ ' + 'INFO'.white + " ] Logging into #{opts[:vcenter]} to get list of vm
 vim = connect_viserver(opts[:vcenter], ad_user, ad_pass)
 vms = get_vm_2(vim)
 
-#find specific VM passed
-clear_line
-logger.info "INFO - Finding #{opts[:vm]} from gathered VMs"
-puts '[ ' + 'INFO'.white + " ] Finding #{opts[:vm]} from gathered VMs"
-
-vm = vms.find { |vm| vm.propSet.find { |prop| prop.name == 'name'}.val == opts[:vm] }
-
-if vm.nil?
+opts[:vm].each do |vm_name|
+  #find specific VM passed
   clear_line
-  logger.info "ERROR - Could not find #{opts[:vm]} on vCenter #{opts[:vcenter]}."
-  puts '[ ' + 'ERROR'.red + " ] Could not find #{opts[:vm]} on vCenter #{opts[:vcenter]}."
-  exit
+  logger.info "INFO - Finding #{vm_name} from gathered VMs"
+  puts '[ ' + 'INFO'.white + " ] Finding #{vm_name} from gathered VMs"
+
+  vm = vms.find { |vm| vm.propSet.find { |prop| prop.name == 'name'}.val == vm_name }
+
+  if vm.nil?
+    clear_line
+    logger.info "ERROR - Could not find #{vm_name} on vCenter #{opts[:vcenter]}."
+    puts '[ ' + 'ERROR'.red + " ] Could not find #{vm_name} on vCenter #{opts[:vcenter]}."
+    exit
+  end
+
+  clear_line
+  logger.info "INFO - Taking snapshot of #{vm_name}"
+  print '[ ' + 'INFO'.white + " ] Taking snapshot of #{vm_name}"
+
+  snap = take_snapshot(vm, opts[:snapshot_name], opts[:snapshot_memory], opts[:quiesce_filesystem], logger)
 end
-
-clear_line
-logger.info "INFO - Taking snapshot of #{opts[:vm]}"
-print '[ ' + 'INFO'.white + " ] Taking snapshot of #{opts[:vm]}"
-
-snap = take_snapshot(vm, opts[:snapshot_name], opts[:snapshot_memory], opts[:quiesce_filesystem], logger)
 
 clear_line
 logger.info "INFO - Script completed"
